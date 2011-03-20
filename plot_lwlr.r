0514f399dea797a1b5f20705e9d08321a5b1107a
Richard Klancer <rpk@pobox.com>
2011-03-20 18:02:26 -0400

    Label title & axes of plot.

--- plot_lwlr.r
+++ plot_lwlr.r
@@ -19,13 +19,14 @@ plot_lwlr <- function(X, y, tau, res) {
     }
   }
   
-  # produces a semi-readable figure...
-  image(r, r, pred, col=heat.colors(res^2));              # plot matrix values with heatmap-like colors
+  # produce a semi-readable figure...
+  image(r, r, pred, col=heat.colors(res^2), xlab="X[1]", ylab="X[2]")     # plot matrix values with heatmap-like colors
   par(new=TRUE);            # redraw next plot on the same figure (that's counterintuitive, huh?)
   plot( res/2 * (1+X[y==0,1]) + 0.5, res/2 * (1+X[y==0,2]) + 0.5, col='white', pch=1, axes=FALSE, xlab='', ylab='');  # pch=1 -> plot points as "o"s
   par(new=TRUE);
   plot( res/2 * (1+X[y==1,1]) + 0.5, res/2 * (1+X[y==1,2]) + 0.5, col='black', pch=4, axes=FALSE, xlab='', ylab='');  # pch=4 -> plot points as "x"s
-  
+  title(sprintf('LWLR surface + training data using tau = %.2f (resolution = %dx%d)', tau, res, res));
+
   pred
   
   # # convert from Matlab:
