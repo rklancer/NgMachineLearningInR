670cbba9ca27515177ffa3eeac6e9ebda55eb9d3
Richard Klancer <rpk@pobox.com>
2011-03-27 12:09:29 -0400

    Fixup problems caused by global contamination.
    Just sourcing doit.r in a fresh workspace should work now.

--- ps1p2_solution/lwlr.r
+++ ps1p2_solution/lwlr.r
@@ -5,28 +5,28 @@ logistic <- function (x) {
 lwlr <- function(X_train, y_train, x, tau) {
   # x = c(x1, x2)
   lambda <- 0.0001
-  min <- 1e-4
+  norm_criterion <- 1e-4
   last_grad_norm <- 1e10
   
   tau2sq <- 2 * tau * tau
   
   xs <- kronecker(t(x), matrix(1, dim(X)[1]))
-  w <-  exp( -1 * as.matrix( rowSums( (xs - X)^2 ) )^(1/2) / tau2sq )
+  w <-  exp( -1 * as.matrix( rowSums( (xs - X_train)^2 ) )^(1/2) / tau2sq )
   theta <- c(1,1)
   
   while (TRUE) {
     h_train <- logistic(X %*% theta)
-    z = w * (y - h_train)
+    z <- w * (y_train - h_train)
     grad <- t(X) %*% z - lambda * theta
     
     grad_norm <- sum( (grad^2)^(1/2) )
-    if ( (grad_norm > last_grad_norm) || (grad_norm < min) ) {
+    if ( (grad_norm > last_grad_norm) || (grad_norm < norm_criterion) ) {
       break;
     }
     last_grad_norm <- grad_norm
     
     D_iis <- as.vector(w * h_train * (1 - h_train))
-    D <- diag(D_iis, nrow = dim(diis)[1])
+    D <- diag(D_iis, nrow = length(D_iis))
     H <- t(X) %*% D %*% X - lambda * diag(2)
     delta_theta <- as.vector(solve(H, grad))
     theta <- theta + delta_theta
